8e168170ab7126f0b91eccf6dfdf4e57985fe576
artem-iliasov <iliasov.artem.27@gmail.com>
2019-04-20 13:15:20 +0300

    use submitting

--- src/components/SendMessageForm.jsx
+++ src/components/SendMessageForm.jsx
@@ -6,20 +6,25 @@ import connect from '../connect';
 
 class SendMessageForm extends React.Component {
   sendMessage = async ({ message }) => {
-    const { sendMessage, reset, currentChannelId, user } = this.props;
+    const {
+      sendMessage,
+      reset,
+      currentChannelId,
+      user,
+    } = this.props;
     await sendMessage(message, currentChannelId, user);
     reset();
   }
 
   render() {
-    const { handleSubmit } = this.props;
+    const { handleSubmit, submitting } = this.props;
     return (
       <div className="container-fluid">
         <form className="form-inline" onSubmit={handleSubmit(this.sendMessage)}>
           <div className="input-group mb-3">
             <Field name="message" required placeholder="message" component="input" type="text" />
             <div className="input-group-append">
-              <button className="btn btn-outline-secondary" type="submit">Button</button>
+              <button className="btn btn-outline-secondary" disabled={submitting} type="submit">Button</button>
             </div>
           </div>
         </form>
